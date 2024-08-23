83340f13d073b598bb2c1c6f1b33756f3a346ea7
ohchamp <90940331+ohchamp@users.noreply.github.com>
2024-08-23 18:19:40 +0900

    test pulumi

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// Create an S3 bucket
+const pulumiStateBucket = new aws.s3.Bucket("pulumi-state-bucket", {
+    bucket: "my-pulumi-state-bucket",  // Ensure this is unique
+    versioning: {
+        enabled: true,
+    },
+    serverSideEncryptionConfiguration: {
+        rule: {
+            applyServerSideEncryptionByDefault: {
+                sseAlgorithm: "AES256",  // Server-side encryption
+            },
+        },
+    },
+});
+
+// Export the bucket name
+export const bucketName = pulumiStateBucket.bucket;
